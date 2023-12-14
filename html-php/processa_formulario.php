4704dee4017f2dbe77c248bc9b39789903530039
lavrud <jr_durval@hotmail.com>
2023-12-14 11:17:11 -0300

    chore: formulário de envio

--- html-php/processa_formulario.php
+++ html-php/processa_formulario.php
@@ -1,3 +1,18 @@
 <?php
-  // SEU CÓDIGO PHP AQUI
-?>
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['nome']) && isset($_POST['email'])) {
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+
+        if (empty($nome) || empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "Erro: Por favor, preencha todos os campos corretamente.";
+        } else {
+            echo "Nome: " . htmlspecialchars($nome) . "<br>";
+            echo "Email: " . htmlspecialchars($email) . "<br>";
+        }
+    } else {
+        echo "Erro: Campos obrigatórios ausentes.";
+    }
+} else {
+    echo "Formulário não enviado.";
+}
